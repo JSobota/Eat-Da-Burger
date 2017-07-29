7147c57942f0391ec7bedf62220cee2f900c5271
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-07-29 11:06:48 -0400

    set up database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+--Creating seed data for DataBase
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheesburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cowboy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whiskey River BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bonzai Burger', false);
