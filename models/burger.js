55ef2026eb40e191ac6415833d26f18811fdfc5b
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-07-29 13:34:48 -0400

    burger.js and handlebars

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+var orm = require('../config/orm.js');
+
+//Create Burger object
+var burger = {
+	//Select all burger table entries
+	selectAll: function(cb) {
+		orm.selectAll('burgers', function(res){
+			cb(res);
+		});
+	},
+
+	insertOne: function(cols, vals, cb){
+		orm.insertOne ('burgers', cols, vals, function(res){
+			cb(res);
+		});
+	},
+
+	updateOne: function(objColVals, condition, function(res){
+		orm.updateOne('burgers', objColVals, condition, function(res){
+			cb(res);
+		});
+	})
+};
+
+
+// Export database for controller (burgerController.js)
+module.exports = burger;
